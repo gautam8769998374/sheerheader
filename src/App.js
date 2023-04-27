fc84f5376e0d9b22187c56a6bf53c3ef1d6d9c9a
Gautam <gautamkharabera4@gmail.com>
2023-04-27 10:35:58 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import UpperHeader from './Components/UpperHeader'
+import "./App.css"
+import LowerHeader from './Components/LowerHeader'
+
+const App = () => {
+    return (
+        <div>
+           <UpperHeader/>
+           <LowerHeader/>
+        </div>
+    )
+}
+
+export default App
---
+++ src/Components/UpperHeader.js
@@ -0,0 +1,26 @@
+import React from "react";
+import logo from "./sai_logo.png";
+import'./UpperHeader.css'
+
+const UpperHeader = () => {
+    return(
+        <div>
+             <header className="upperHeader">
+      <div className="logo">
+        <img src={logo} width={200} height={40} alt="Sheer Analytics and Insights logo" />
+      </div>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+      <div className="contact-info">
+        {/* Add your contact info */}
+      </div>
+      <div className="search-box">
+        <input type="text" placeholder="Search" />
+        <button type="submit" className="search-btn"><i className="fa fa-search"></i></button>
+      </div>
+    </header>
+    <hr class="hr"></hr>
+        </div>
+    )
+}
+
+export default UpperHeader
